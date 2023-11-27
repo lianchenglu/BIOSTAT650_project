a36efc7109271e2dd619b7209b6306a8e9196455
liancheng <chenggg@umich.edu>
2023-11-27 12:15:12 -0500

    p1

--- part1.R
+++ part1.R
@@ -1,5 +1,5 @@
-rm(list = ls())
-gc()
+# rm(list = ls())
+# gc()
 set.seed(123)
 library(car)
 library(ggplot2)
@@ -54,27 +54,24 @@ ggplot(df4, aes(x = BMI_Group, y = SleepHrsNight, fill = BMI_Group)) +
   theme_minimal()
 
 
-ggplot(data_complete, aes(x = Gender, y = BMI, fill = Gender)) +  # 这里使用 Gender 作为 x 轴分类，您可以根据需要更改
+ggplot(data_complete, aes(x = Gender, y = BMI, fill = Gender)) +
   geom_boxplot() +
-  geom_jitter(width = 0.2, alpha = 0.5, color = "black") +  # 显示每个数据点
+  geom_jitter(width = 0.2, alpha = 0.5, color = "black") +
   labs(title = "BMI Distribution by Gender", x = "Gender", y = "BMI")
 
 library(ggplot2)
 
-# 添加一个新的分类变量，基于 BMI 是否等于 30
 data_complete$BMI_Category <- ifelse(data_complete$BMI >= 30, "BMI >= 30", "BMI < 30")
 
-# 使用 ggplot 创建箱形图
+
 ggplot(data_complete, aes(x = BMI_Category, y = SleepHrsNight, fill = BMI_Category)) +
   geom_boxplot() +
-  geom_jitter(width = 0.2, alpha = 0.5, color = "black") +  # 显示每个数据点
+  geom_jitter(width = 0.2, alpha = 0.5, color = "black") +
   labs(title = "Sleep Hours by BMI Category", x = "BMI Category", y = "Hours of Sleep")
 
 
-data_incomplete$BMI_Category <- ifelse(data_incomplete$BMI >= 30, "BMI >= 30", "BMI < 30")
-
-# 使用 ggplot 创建箱形图
-ggplot(data_incomplete, aes(x = BMI_Category, y = SleepHrsNight, fill = BMI_Category)) +
+ggplot(data_complete, aes(x = BMI_Category, y = DaysMentHlthBad, fill = BMI_Category)) +
   geom_boxplot() +
-  geom_jitter(width = 0.2, alpha = 0.5, color = "black") +  # 显示每个数据点
-  labs(title = "Sleep Hours by BMI Category", x = "BMI Category", y = "Hours of Sleep")
+  geom_jitter(width = 0.2, alpha = 0.5, color = "black") +
+  labs(title = "DaysMentHlthBad by BMI Category", x = "BMI Category", y = "DaysMentHlthBad")
+
